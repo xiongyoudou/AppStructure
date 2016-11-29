eecd0849adb1f3f056c9faae89090f2bca9c4c0d
xiongyoudou <xiongyoudou@xyd.local>
2016-11-29 18:20:45 +0800

    update yykit

--- HampooHomeClient/Class(业务模块)/主页/View/Timeline/TimelineVCtrl.m
+++ HampooHomeClient/Class(业务模块)/主页/View/Timeline/TimelineVCtrl.m
@@ -11,6 +11,9 @@
 #import "TimelineLayout.h"
 #import "TimelineCell.h"
 #import "YYModel.h"
+#import "YYPhotoGroupView.h"
+#import "YYSimpleWebViewController.h"
+#import "NSString+XYDURLEncode.h"
 
 @interface TimelineVCtrl ()<UITableViewDelegate,UITableViewDataSource,TLCellDelegate>
 
@@ -100,11 +103,6 @@
 }
 
 
-/// 点击了转发内容
-- (void)cellDidClickRetweet:(TLCell *)cell {
-    
-}
-
 /// 点击了 Cell 菜单
 - (void)cellDidClickMenu:(TLCell *)cell {
     
@@ -112,11 +110,11 @@
 
 /// 点击了下方 Tag
 - (void)cellDidClickTag:(TLCell *)cell {
-//    TLTag *tag = cell.statusView.layout.model.tagStruct.firstObject;
-//    NSString *url = tag.tagScheme; // sinaweibo://... 会跳到 Weibo.app 的。。
-//    YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
-//    vc.title = tag.tagName;
-//    [self.navigationController pushViewController:vc animated:YES];
+    TLTag *tag = cell.statusView.layout.model.tagStruct.firstObject;
+    NSString *url = tag.tagScheme; // sinaweibo://... 会跳到 Weibo.app 的。。
+    YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
+    vc.title = tag.tagName;
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 /// 点击了关注
@@ -126,102 +124,102 @@
 
 /// 点击了用户
 - (void)cell:(TLCell *)cell didClickUser:(TLUser *)user {
-//    if (user.userId == 0) return;
-//    NSString *url = [NSString stringWithFormat:@"http://m.weibo.cn/u/%lld",user.userID];
-//    YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
-//    [self.navigationController pushViewController:vc animated:YES];
+    if (user.userID == 0) return;
+    NSString *url = [NSString stringWithFormat:@"http://m.weibo.cn/u/%lld",user.userID];
+    YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 /// 点击了图片
 - (void)cell:(TLCell *)cell didClickImageAtIndex:(NSUInteger)index {
-//    UIView *fromView = nil;
-//    NSMutableArray *items = [NSMutableArray new];
-//    TLModel *model = cell.statusView.layout.model;
-//    NSArray<TLPicture *> *pics = model.pics;
-//    
-//    for (NSUInteger i = 0, max = pics.count; i < max; i++) {
-//        UIView *imgView = cell.statusView.picViews[i];
-//        TLPicture *pic = pics[i];
-//        TLPictureMetadata *meta = pic.largest.badgeType == TLPictureBadgeTypeGIF ? pic.largest : pic.large;
-//        YYPhotoGroupItem *item = [YYPhotoGroupItem new];
-//        item.thumbView = imgView;
-//        item.largeImageURL = meta.url;
-//        item.largeImageSize = CGSizeMake(meta.width, meta.height);
-//        [items addObject:item];
-//        if (i == index) {
-//            fromView = imgView;
-//        }
-//    }
-//    
-//    YYPhotoGroupView *v = [[YYPhotoGroupView alloc] initWithGroupItems:items];
-//    [v presentFromImageView:fromView toContainer:self.navigationController.view animated:YES completion:nil];
+    UIView *fromView = nil;
+    NSMutableArray *items = [NSMutableArray new];
+    TLModel *model = cell.statusView.layout.model;
+    NSArray<TLPicture *> *pics = model.pics;
+    
+    for (NSUInteger i = 0, max = pics.count; i < max; i++) {
+        UIView *imgView = cell.statusView.picViews[i];
+        TLPicture *pic = pics[i];
+        TLPictureMetadata *meta = pic.largest.badgeType == TLPictureBadgeTypeGIF ? pic.largest : pic.large;
+        YYPhotoGroupItem *item = [YYPhotoGroupItem new];
+        item.thumbView = imgView;
+        item.largeImageURL = meta.url;
+        item.largeImageSize = CGSizeMake(meta.width, meta.height);
+        [items addObject:item];
+        if (i == index) {
+            fromView = imgView;
+        }
+    }
+    
+    YYPhotoGroupView *v = [[YYPhotoGroupView alloc] initWithGroupItems:items];
+    [v presentFromImageView:fromView toContainer:self.navigationController.view animated:YES completion:nil];
 }
 
 /// 点击了 Label 的链接
 - (void)cell:(TLCell *)cell didClickInLabel:(YYLabel *)label textRange:(NSRange)textRange {
-//    NSAttributedString *text = label.textLayout.text;
-//    if (textRange.location >= text.length) return;
-//    YYTextHighlight *highlight = [text attribute:YYTextHighlightAttributeName atIndex:textRange.location];
-//    NSDictionary *info = highlight.userInfo;
-//    if (info.count == 0) return;
-//    
-//    if (info[kTLLinkHrefName]) {
-//        NSString *url = info[kTLLinkHrefName];
-//        YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
-//        [self.navigationController pushViewController:vc animated:YES];
-//        return;
-//    }
-//    
-//    if (info[kTLLinkURLName]) {
-//        TLURL *url = info[kTLLinkURLName];
-//        TLPicture *pic = url.pics.firstObject;
-//        if (pic) {
-//            // 点击了文本中的 "图片链接"
-//            YYTextAttachment *attachment = [label.textLayout.text attribute:YYTextAttachmentAttributeName atIndex:textRange.location];
-//            if ([attachment.content isKindOfClass:[UIView class]]) {
-//                YYPhotoGroupItem *info = [YYPhotoGroupItem new];
-//                info.largeImageURL = pic.large.url;
-//                info.largeImageSize = CGSizeMake(pic.large.width, pic.large.height);
-//                
-//                YYPhotoGroupView *v = [[YYPhotoGroupView alloc] initWithGroupItems:@[info]];
-//                [v presentFromImageView:attachment.content toContainer:self.navigationController.view animated:YES completion:nil];
-//            }
-//            
-//        } else if (url.oriURL.length){
-//            YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url.oriURL]];
-//            [self.navigationController pushViewController:vc animated:YES];
-//        }
-//        return;
-//    }
-//    
-//    if (info[kTLLinkTagName]) {
-//        TLTag *tag = info[kTLLinkTagName];
-//        NSLog(@"tag:%@",tag.tagScheme);
-//        return;
-//    }
-//    
-//    if (info[kTLLinkTopicName]) {
-//        TLTopic *topic = info[kTLLinkTopicName];
-//        NSString *topicStr = topic.topicTitle;
-//        topicStr = [topicStr stringByURLEncode];
-//        if (topicStr.length) {
-//            NSString *url = [NSString stringWithFormat:@"http://m.weibo.cn/k/%@",topicStr];
-//            YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
-//            [self.navigationController pushViewController:vc animated:YES];
-//        }
-//        return;
-//    }
-//    
-//    if (info[kTLLinkAtName]) {
-//        NSString *name = info[kTLLinkAtName];
-//        name = [name stringByURLEncode];
-//        if (name.length) {
-//            NSString *url = [NSString stringWithFormat:@"http://m.weibo.cn/n/%@",name];
-//            YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
-//            [self.navigationController pushViewController:vc animated:YES];
-//        }
-//        return;
-//    }
+    NSAttributedString *text = label.textLayout.text;
+    if (textRange.location >= text.length) return;
+    YYTextHighlight *highlight = [text yy_attribute:YYTextHighlightAttributeName atIndex:textRange.location];
+    NSDictionary *info = highlight.userInfo;
+    if (info.count == 0) return;
+    
+    if (info[kTLLinkHrefName]) {
+        NSString *url = info[kTLLinkHrefName];
+        YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
+        [self.navigationController pushViewController:vc animated:YES];
+        return;
+    }
+    
+    if (info[kTLLinkURLName]) {
+        TLURL *url = info[kTLLinkURLName];
+        TLPicture *pic = url.pics.firstObject;
+        if (pic) {
+            // 点击了文本中的 "图片链接"
+            YYTextAttachment *attachment = [label.textLayout.text yy_attribute:YYTextAttachmentAttributeName atIndex:textRange.location];
+            if ([attachment.content isKindOfClass:[UIView class]]) {
+                YYPhotoGroupItem *info = [YYPhotoGroupItem new];
+                info.largeImageURL = pic.large.url;
+                info.largeImageSize = CGSizeMake(pic.large.width, pic.large.height);
+                
+                YYPhotoGroupView *v = [[YYPhotoGroupView alloc] initWithGroupItems:@[info]];
+                [v presentFromImageView:attachment.content toContainer:self.navigationController.view animated:YES completion:nil];
+            }
+            
+        } else if (url.oriURL.length){
+            YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url.oriURL]];
+            [self.navigationController pushViewController:vc animated:YES];
+        }
+        return;
+    }
+    
+    if (info[kTLLinkTagName]) {
+        TLTag *tag = info[kTLLinkTagName];
+        NSLog(@"tag:%@",tag.tagScheme);
+        return;
+    }
+    
+    if (info[kTLLinkTopicName]) {
+        TLTopic *topic = info[kTLLinkTopicName];
+        NSString *topicStr = topic.topicTitle;
+        topicStr = [topicStr xyd_urlEncode];
+        if (topicStr.length) {
+            NSString *url = [NSString stringWithFormat:@"http://m.weibo.cn/k/%@",topicStr];
+            YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
+            [self.navigationController pushViewController:vc animated:YES];
+        }
+        return;
+    }
+    
+    if (info[kTLLinkAtName]) {
+        NSString *name = info[kTLLinkAtName];
+        name = [name xyd_urlEncode];
+        if (name.length) {
+            NSString *url = [NSString stringWithFormat:@"http://m.weibo.cn/n/%@",name];
+            YYSimpleWebViewController *vc = [[YYSimpleWebViewController alloc] initWithURL:[NSURL URLWithString:url]];
+            [self.navigationController pushViewController:vc animated:YES];
+        }
+        return;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
